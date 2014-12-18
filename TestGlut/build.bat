0149bdffd85ddedf33fbedf7b4f3a7a988f8b5c6
yafacex <yafacex@gmail.com>
2014-12-18 13:23:17 +0800

    gl

--- TestGlut/build.bat
+++ TestGlut/build.bat
@@ -1,2 +1,2 @@
-gcc -o haha.exe hello.c -lglut32cu -lglew32 -lopengl32
+gcc -o haha.exe hello.cpp -lglut32cu -lglew32 -lopengl32
 haha
--- TestGlut/hello.cpp
+++ TestGlut/hello.cpp
@@ -3,7 +3,7 @@
 #include <GL/glew.h>  
 #include <GL/glut.h> 
 #include <stdio.h>
-#include "YGL/ShaderMgr.h"
+#include "YGL/SingleShader.h"
 void display(void)  
 {  
 
